785a52fc32b2409bdd1996f0d14d1c3ee9deb49a
Max <kirilukm157@gmail.com>
2026-02-19 17:09:56 +0200

    update

--- lab1/script.js
+++ lab1/script.js
@@ -1,15 +1,13 @@
-// 1. Отримуємо доступ до елементів за допомогою querySelector (вимога варіанту)
 const title = document.querySelector('#main-title');
 const button = document.querySelector('.action-btn');
 
-// 2. Створюємо функцію для обробки події
 function processEvent() {
-    // Виводимо повідомлення у елемент h1 (вимога завдання)
+
     title.innerText = "Hello world!";
+
+    button.innerText = "Молодець)";
     
-    // Виводимо ім'я в консоль як warn (вимога варіанту)
     console.warn("Максим");
 }
 
-// 3. Призначаємо івент onmousemove для кнопки (вимога варіанту та завдання)
 button.onmousemove = processEvent;
